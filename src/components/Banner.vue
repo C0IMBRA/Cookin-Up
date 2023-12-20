f6f223f1d2b702671c44a2616b0b84a0348ef511
Gabriel Coimbra <coimbragabriel85@gmail.com>
2023-12-19 23:21:01 -0300

    Primeiro commit

---
+++ src/components/Banner.vue
@@ -0,0 +1,19 @@
+<template>
+    <header class="banner">
+      <div class="apresentacao">
+        <img src="../assets/imagens/logo.svg" alt="Logo do Cookin' Up"> 
+  
+        <p class="cabecalho-lg frase-cabecalho">
+          <span class="texto-verde">Um banquete de ideias para</span>
+          despertar o chef que há em você!
+        </p>
+  
+        <p class="subtitulo-lg">Explore novas receitas todos os dias com os
+          ingredientes que estão ao seu alcance!
+        </p>
+      </div>
+      <img src="../assets/imagens/foto-banner.png" alt="Foto de uma mulher cozinhando." class="foto-banner">
+    </header>
+</template>
+<style scoped>
+</style>
